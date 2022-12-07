78b68c97466e44e10017a43802ad00277c45d3a1
Djalma Filho <designer.djalma@gmail.com>
2022-12-07 12:22:58 -0300

    Finalizacoes

---
+++ interfaceQuestao1/funcoes.py
@@ -0,0 +1,27 @@
+import string
+import getpass
+
+def retornaNomeUsuario() -> str:
+    """Retorna o nome do usuário."""
+    return getpass.getuser()
+
+def descriptografarMensagem(mensagem: str) -> str:
+    mensagem = mensagem.replace("[","").replace("]","").replace(" ","").split(",")
+    chave = {str(i):v for i,v in enumerate(f" {string.printable}")}
+    return ''.join([chave[i] for i in mensagem if i in chave.keys()])
+
+def criptografarMensagem(mensagem: str) -> str:
+    chave = {v:i for i, v in enumerate(f" {string.printable}")}
+    return ', '.join([str(chave[i]) for i in [i for i in mensagem] if i])
+
+def tradutorDeMensagem(msgSecreta: str) -> str:
+    """Traduz uma mensagem secreta em normal."""
+    traduzido = descriptografarMensagem(msgSecreta)
+    print(f"\nMensagem traduzida: {traduzido}")
+    return traduzido
+
+def destradutorDeMensagem(msgNormal: str) -> str:
+    """Traduz uma mensagem normal em secreta."""
+    destraduzido = criptografarMensagem(msgNormal)
+    print(f"\nMensagem secreta: {destraduzido}")
+    return destraduzido
--- interfaceQuestao1/main.py
+++ interfaceQuestao1/main.py
@@ -1,18 +1,30 @@
 from kivymd.app import MDApp
 from kivy.core.window import Window
 from kivy.uix.screenmanager import Screen
-import getpass
+
+from funcoes import tradutorDeMensagem
+from funcoes import destradutorDeMensagem
+from funcoes import retornaNomeUsuario
 
 fatorEscala = 1.4
 Window.size = (350 * fatorEscala, 580 * fatorEscala)
+
 
 class TelaInicial(Screen):
     pass
 
 class TelaPrincipal(Screen):
-    def on_enter(self, *args):
-        self.ids.nomeDetetive.text = f"Detetive: {getpass.getuser()}"
+    def on_enter(self, *args) -> None:
+        self.ids.nomeDetetive.text = f"Detetive: {retornaNomeUsuario()}"
         pass
+
+    def traduzir(self) -> None:
+        self.ids.mensagemFinal.text = tradutorDeMensagem(
+            self.ids.mensagemInicial.text)
+
+    def codificar(self) -> None:
+        self.ids.mensagemFinal.text = destradutorDeMensagem(
+            self.ids.mensagemInicial.text)
 
 class TelaAjuda(Screen):
     pass
@@ -20,5 +32,8 @@ class TelaAjuda(Screen):
 class InterfaceApp(MDApp):
     pass
 
+
+
 if __name__ == "__main__":
     InterfaceApp().run()
+
